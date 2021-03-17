c589f4fba26e2b17249c6f3943fac81062b1c1da
Meiirzhan <54979320+Meiirzhan00@users.noreply.github.com>
2021-03-17 21:21:04 +0600

    Create ReverseInteger.cpp

---
+++ ReverseInteger.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Input number : ";
+  cin >> n;
+  int result=0;
+  int abs_n=abs(n);
+  while(abs_n != 0){
+    
+    int reminder=abs_n%10;
+    abs_n=abs_n/10;
+    int temp=result*10+reminder;
+    result=temp;
+    
+  }
+
+  if (n<0){
+    result = -1*result;
+  }
+  
+  cout << result <<endl;
+  
+}
