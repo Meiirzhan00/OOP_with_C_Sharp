657eee19ab85ae017168c1c3b55b676706512512
Meiirzhan <54979320+Meiirzhan00@users.noreply.github.com>
2021-04-22 23:56:35 +0600

    Create 13th_week.cs

---
+++ 13th_week.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("\nBolshe alymyn engiz : ");
+                int alym = int.Parse(Console.ReadLine());
+                Console.Write("Bolshek bolimin engiz : ");
+                int bolim = int.Parse(Console.ReadLine());
+                double mani = alym/bolim;
+
+                if ( bolim>alym)
+                {
+                    throw new Exception("Bul durys bolshek. Bizge burys bolshek kerek !\n");
+                }
+                
+                else 
+                {
+                    Console.WriteLine($"Butin bolik : {alym/bolim}\n");
+                }
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine($"Katelik : {e.Message}\n");
+            }
+        }
+    }    
+}
